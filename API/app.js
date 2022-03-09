f6a5ddd0ff9cf086e805f7390c934d1dd5d0ffb0
Fran <fransanxenxo@gmail.com>
2022-03-09 16:26:15 +0000

    feat: configure database connection

--- API/app.js
+++ API/app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
+
+require('./config/db.config')
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Aplication running 🏃🏼‍♂️ at port 🏔️ ${port}`))
