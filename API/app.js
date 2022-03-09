1ee45f6a3d60ee44c38e626fdd402f1cd9fdab7b
Fran <fransanxenxo@gmail.com>
2022-03-09 16:09:44 +0000

    chore: api scaffolding

---
+++ API/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Aplication running 🏃🏼‍♂️ at port 🏔️ ${port}`))
