2c0fb834f7136cb1fe858d7326780580731da093
Fran <fransanxenxo@gmail.com>
2022-03-14 16:45:28 +0000

    feat: error handling

--- API/config/db.config.js
+++ API/config/db.config.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/SHOP_BIKES';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/bike-shop';
 
 mongoose
-    .connect(MONGODB_URI, { userNewUrlParser: true, userUnifiedTopology: true})
-    .then(() => console.ingo(`Succesfully connected to the database ${MONGODE_URI}`))
-    .catch((error) => {
-        console.error(`An error ocurred trying to connect to de database ${MONGODB_URI}, error`);
-        process.exit(0)
-    } )
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
+  .catch((error) => {
+    console.error(`An error ocurred trying to connect to de database ${MONGODB_URI}`, error);
+    process.exit(0);
+  });
 
-    process.on('SIGINT', function() {
-         mongoose.connection.close(function () {
-             console.log('Mongoose disconnected on app termination');
-             process.exit(0)
-         })
-    })
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected on app termination');
+    process.exit(0);
+  });
+});
 
-    module.exports.connectionUrl = MONGODB_URI;
+module.exports.connectionUrl = MONGODB_URI;
