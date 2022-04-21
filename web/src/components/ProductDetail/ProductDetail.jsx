f84d333c674bcdbb6398a748e38662ae2e297dd4
Fran <fransanxenxo@gmail.com>
2022-04-21 19:42:07 +0100

    feat:produc detail  page responsive end

--- web/src/components/ProductDetail/ProductDetail.jsx
+++ web/src/components/ProductDetail/ProductDetail.jsx
@@ -39,7 +39,7 @@ function ProductDetail() {
         <>
             <hr />
             <div className='card-div'>
-                <div className=" card mb-5 mt-5" style={{ maxWidth: '840px' }}>
+                <div className=" card mb-5 mt-5" >
                     <div className="row g-0">
                         <div className="product-image col-md-6">
                             <img src={product.image} className="img-fluid rounded-start mt-5" alt={product.id} />
