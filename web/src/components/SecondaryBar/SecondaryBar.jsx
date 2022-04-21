6ae46f26427a189b8d5248f0eb209e0d4494ebb4
Fran <fransanxenxo@gmail.com>
2022-04-21 18:49:53 +0100

    new web

--- web/src/components/SecondaryBar/SecondaryBar.jsx
+++ web/src/components/SecondaryBar/SecondaryBar.jsx
@@ -11,7 +11,7 @@ function Secondarybar() {
     }
 
     return (
-        <div className='secondary-bar mt-2'>
+        <div className='secondary-bar mt-2 rounded-3 '>
              <div className='form-search'>
           <form onSubmit={handleSearch} className="d-flex p-2 secondary-form mt-3 ">
             <input name='search' className="form-control me-2" type="search" placeholder="Busqueda de productos" aria-label="Search" />
